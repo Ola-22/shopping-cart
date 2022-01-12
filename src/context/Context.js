9f530df2147c52ecd4dd5448dcf1bf3bc13a77d1
Ola-22 <olanazly@gmail.com>
2022-01-12 23:46:58 +0200

    add useReducer from react

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,7 +1,21 @@
-import { createContext } from "react";
+import { createContext, useReducer, useContext } from "react";
+import products from "../products";
+import { cartReducer } from "./Reducers";
 
 export const Cart = createContext();
 
-export default function Context({ children }) {
-  return <Cart.Provider>{children}</Cart.Provider>;
+function Context({ children }) {
+  console.log(products);
+
+  const [state, dispatch] = useReducer(cartReducer, {
+    products: products,
+    cart: [],
+  });
+  return <Cart.Provider value={{ state, dispatch }}>{children}</Cart.Provider>;
 }
+
+export const CartState = () => {
+  return useContext(Cart);
+};
+
+export default Context;
