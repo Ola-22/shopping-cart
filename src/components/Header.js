003b732639de8cc3ae059e3f71767816f23e61db
Ola-22 <olanazly@gmail.com>
2022-01-13 14:08:27 +0200

    Add cart from state context file

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,8 +8,13 @@ import {
 } from "react-bootstrap";
 import { FaShoppingCart } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { CartState } from "../context/Context";
 
 export default function Header() {
+  const {
+    state: { cart },
+  } = CartState();
+
   return (
     <Navbar bg="dark" variant="dark" style={{ height: "80px" }}>
       <Container>
@@ -28,10 +33,28 @@ export default function Header() {
           <Dropdown>
             <Dropdown.Toggle variant="success">
               <FaShoppingCart color="white" fontSize="25px" />
-              <Badge bg="success">{10}</Badge>
+              <Badge bg="success">{cart.length}</Badge>
             </Dropdown.Toggle>
             <Dropdown.Menu style={{ minWidth: 370 }}>
-              <span style={{ padding: 10 }}>Cart is Empty!</span>
+              {cart.length > 0 ? (
+                <>
+                  {cart.map((product) => (
+                    <span className="cartItem" key={product.id}>
+                      <img
+                        src={product.image}
+                        className="cartItemImg"
+                        alt={product.name}
+                      />
+                      <div className="cartItemDetail">
+                        <span>{product.name}</span>
+                        <span>$ {product.price}</span>
+                      </div>
+                    </span>
+                  ))}
+                </>
+              ) : (
+                <span style={{ padding: 10 }}>Cart is Empty!</span>
+              )}
             </Dropdown.Menu>
           </Dropdown>
         </Nav>
